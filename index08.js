6de331ff4f3f98019f68ac263ebe78c0938a4301
Emanuel Sousa <mano.sousa@gmail.com>
2025-04-17 17:00:40 -0300

    commit 02

--- index08.js
+++ index08.js
@@ -1,2 +1,13 @@
 // Receba três notas e faça a média aritmética.
 
+    // utiliza o parsefloat para pegar todas as casas decimais que o usuário vai inserir.
+
+
+let a = parseFloat (prompt("Digite a primeira nota"))
+let b = parseFloat (prompt("Digite a segunda nota"))
+let c = parseFloat (prompt("Digite a terceira nota"))
+let media = ((a + b + c)/3)
+
+ console.log ("A média aritmética entre a, b e c é: " + media.toFixed(2))
+
+ // media.toFixed serve para fixar o número de casas decimais. No exemplo aqui é 2. A quantidade de casa é indicada dentro do parenteses. 
