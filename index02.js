2b24d93d07e8d17e0d25f7f8c4e5c5670cc7a5e4
Emanuel Sousa <mano.sousa@gmail.com>
2025-04-17 09:44:56 -0300

    commit 02 do exercício 02

--- index02.js
+++ index02.js
@@ -1,1 +1,7 @@
 //Exercício 02 - Crie um programa que solicite ao usuário dois números e exiba o resultado da soma, subtração, multiplicação e divisão desses números.
+
+let numero1 = 10
+let numero2 = 5
+console.log ("A soma é: " + (numero1 + numero2))
+console.log ("O produto é: " + (numero1*numero2))
+console.log ("O quociente é: " + (numero1/numero2))
