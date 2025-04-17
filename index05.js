429ab1be5717519bb4c1b7f1fb4ab55d8e64a3cf
Emanuel Sousa <mano.sousa@gmail.com>
2025-04-17 15:38:53 -0300

    commit 01

---
+++ index05.js
@@ -0,0 +1,5 @@
+// Faça um programa que recebe dois números inteiros e calcule o dobro de seus valores
+let x = 3
+let y = 6
+console.log ("O dobro de x é: " + (2*x))
+console. log ("O dobro de y é: " + (2*y))
