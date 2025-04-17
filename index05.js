82f3d15d8cf845bfc7a3e79b0ac11025888f72ba
Emanuel Sousa <mano.sousa@gmail.com>
2025-04-17 15:45:15 -0300

    commit 03

--- index05.js
+++ index05.js
@@ -2,5 +2,7 @@
 // não consegui usar o prompt. Qual o motivo? Sempre está dando erro quando uso o prompt aqui no Visual Studio Code
 let x = 3
 let y = 6
-console.log ("O dobro de x é: " + (2*x))
-console. log ("O dobro de y é: " + (2*y))
+let dobrox = 2*x
+let dobroy = 2*y
+console.log ("O dobro de x é: " + dobrox)
+console. log ("O dobro de y é: " + dobroy)
