b9307b941ad4124a24b49efb23955cf684c621d4
Emanuel Sousa <mano.sousa@gmail.com>
2025-04-17 16:26:57 -0300

    index06.js

---
+++ index06.js
@@ -0,0 +1,19 @@
+// Faça um programa que mostra na saída o resultado de 1356 + 4562, 5 * 85, 1265 - 154 e 1862/2.
+let x = 1356;
+let y = 4562;
+let z = 5;
+let w = 85;
+let a = 1265;
+let b = 154;
+let c = 1862;
+let d = 2;
+let resultado1 = x + y;
+let resultado2 = z * w;
+let resultado3 = a - b;
+let resultado4 = c/d;
+
+console.log ("O valor de (1356 + 4562) é: "+ resultado1);
+console.log ("O valor de (5 * 85) é: "+ resultado2);
+console.log ("O valor de (1265 - 154) é: "+ resultado3);
+console.log ("O valor de (1862/2) é: "+ resultado4);
+
