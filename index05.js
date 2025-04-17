461f4ae89c90a00f80baa1f198652aa24192a196
Emanuel Sousa <mano.sousa@gmail.com>
2025-04-17 15:42:03 -0300

    commit 02

--- index05.js
+++ index05.js
@@ -1,4 +1,5 @@
 // Faça um programa que recebe dois números inteiros e calcule o dobro de seus valores
+// não consegui usar o prompt. Qual o motivo? Sempre está dando erro quando uso o prompt aqui no Visual Studio Code
 let x = 3
 let y = 6
 console.log ("O dobro de x é: " + (2*x))
