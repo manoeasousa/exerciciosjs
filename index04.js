cea1c3a8ee0117e5f32badf8e3c9434571ba8ee2
Emanuel Sousa <mano.sousa@gmail.com>
2025-04-17 15:27:56 -0300

    commit 01

---
+++ index04.js
@@ -0,0 +1,5 @@
+// Escreva um código para somar 10 + 13, 123 + 327 e 4215 + 8962
+
+console.log ("A soma de 10 + 13 é: " + (10+13))
+console.log ("A soma de 10 + 13 é: " + (123+327))
+console.log ("A soma de 10 + 13 é: " + (4215+8962))
