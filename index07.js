3d6dab74fd04c048aa5d86ad56e0c82cb3267786
Emanuel Sousa <mano.sousa@gmail.com>
2025-04-17 16:44:05 -0300

    commit 01

---
+++ index07.js
@@ -0,0 +1,8 @@
+// Crie três variáveis a, b e c, e tribua respectivamente os valores 1.3, 3.4 e 5.4. Faça um programa que calcule a média aritmética.
+
+let a = 1.3
+let b = 3.4
+let c = 5.5
+let media = ((a + b + c)/3)
+
+ console.log ("A média aritmética entre a, b e c é: " + media)
