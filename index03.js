436dfc3d9933a6c1fc225e350a78d09a9399c5d6
Emanuel Sousa <mano.sousa@gmail.com>
2025-04-17 16:36:19 -0300

    commit 01

--- index03.js
+++ index03.js
@@ -1,1 +1,6 @@
-// Exercício 03 - Escreva um programa que pergunte ao usuário sua idade e imprima se ele é maior ou menor de idade.
+// Exercício 03 - Escreva um programa que pergunte ao usuário sua idade e imprima se ele é maior ou menor de idade
+
+
+prompt ("Informe a sua idade")
+read (idade)
+
